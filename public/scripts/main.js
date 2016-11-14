da9a736040072646093ba7220afd1752f789ff69
Joy Liu <liukang.joy@gmail.com>
2016-11-14 08:45:58 -0500

    filtering function param change

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -8,14 +8,11 @@ droneApp.getDrones = $.ajax({
     method: 'GET',
     dataType: 'jsonp'
 });
-
 $.when(droneApp.getDrones).then(function (data) {
     data.strike.map(function (result) {
         // group data by country + year
         result.date = result.date.split('-')[0];
-
         // keep detailed geolocation information
-
         // use funciton in console.log() to get the years of each drone strike
         // console.log(result.country)
 
@@ -25,7 +22,6 @@ $.when(droneApp.getDrones).then(function (data) {
 
         // display information
     });
-
     $('input[type=checkbox]').on('change', function () {
         function getCheckedInputValue(param) {
             return $('input[name=' + param + ']:checked').map(function (input, value) {
@@ -35,24 +31,22 @@ $.when(droneApp.getDrones).then(function (data) {
         var checkedDates = getCheckedInputValue('date');
         var checkedCountries = getCheckedInputValue('country');
         var date = 'date';
-        var countru = 'country';
+        var country = 'country';
         var defaultCoutries = ['yemen', 'somalia', 'pakistan'];
         var defaultDates = ['2002', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'];
-        console.log('result2', result);
-        var filteringResult = function filteringResult(checkedValues, defaultValues, key) {
-            console.log('is it working');
+        var filteringResult = function(checkedValues, defaultValues, key) {
             if (checkedValues.length > 0) {
                 // MAP OUT DATA.STRIKE HERE
-                return data.strike.filter(function () {
+                return data.strike.filter(function (result) {
                     return result.key === checkedValues;
                 });
             } else {
-                return data.strike.filter(function () {
-                    return result.key === checkedValues;
+                return data.strike.filter(function(result) {
+                    return result.key === defaultValues;
                 });
             }
         };
         filteringResult(checkedDates, defaultDates, date);
-        filteringResult(checkedCountries, defaultCoutries, country);
+        filteringResult(checkedCountries, defaultCoutries, country );
     });
 });
